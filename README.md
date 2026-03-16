fc9bf37860ff55fd5d8781ccf6533383e7137fd2
m0ssiness <timofej.bozhenko@yandex.ru>
2026-03-16 20:54:38 +0100

    initial: alpha version

---
+++ README.md
@@ -0,0 +1,3 @@
+# LC-Circuit visualised with Julia (Lixor)
+Visualisation features animated dI/dt graph brought in connection with magnetic field according to B ~ I
+![animation](LC.gif)
---
+++ sim.jl
@@ -0,0 +1,133 @@
+# L * omega = 1 / (omega * C)
+
+using Luxor
+
+include(dirname(pathof(Luxor)) * "/play.jl")
+
+const LInd = 700
+const CCap = 0.00005
+const UVolt = 80
+UCond = UVolt
+Q = CCap * UVolt
+omega = 1 / sqrt(LInd * CCap)
+period = (2 * pi) / omega
+print(period)
+
+q = t -> Q * cos(omega * t); # derives from differential equation of an LC-circuit
+I = t -> omega * Q * sin(omega * t)
+dI = t -> omega * omega * Q * cos(omega * t)
+
+const WI = 1200
+const HE = 800
+const MA = 50
+const STX = 250
+const STY = 1800
+const CIRCUITWIDTH = 400
+const CIRCUITHEIGHT = 400
+
+function drawAxis()
+    sethue("black")
+    fontsize(20)
+    text("Änderung der Stromstärke dI/dt", 30, -HE / 2 + MA + 20)
+    line(Point(-MA, 0), Point(period * STX + MA, 0))
+    line(Point(0, -HE / 2 + MA), Point(0, HE / 2 - MA))
+    strokepath()
+    fontsize(15)
+    for i in 0:0.2:(trunc(period * 5)*0.2+0.2)
+        circle(Point(0 + i * STX, 0), 3, :fill)
+        text(string(i), Point(0 + i * STX - 7, 0 + 15))
+    end
+    circle(Point(0, -STY * omega * omega * Q), 3, :fill)
+    text(join([string(omega * omega * Q), " C/s^2"]), Point(12, -STY * omega * omega * Q))
+end
+
+function drawCircuit()
+    sethue("black")
+    move(Point(WI - MA - MA - 40 - CIRCUITWIDTH, -10))
+    line(Point(WI - MA - MA - 40 - CIRCUITWIDTH + 30, -10))
+    line(Point(WI - MA - MA - 40 - CIRCUITWIDTH - 30, -10))
+    move(Point(WI - MA - MA - 40 - CIRCUITWIDTH, -10))
+    line(Point(WI - MA - MA - 40 - CIRCUITWIDTH, -CIRCUITHEIGHT / 2))
+    move(Point(WI - MA - MA - 40 - CIRCUITWIDTH, -CIRCUITHEIGHT / 2))
+    line(Point(WI - MA - MA - 40, -CIRCUITHEIGHT / 2))
+    move(Point(WI - MA - MA - 40, -CIRCUITHEIGHT / 2))
+    line(Point(WI - MA - MA - 40, -30))
+    move(Point(WI - MA - MA - 40, -30))
+    for i in -30:10:20
+        arc2r(Point(WI - MA - MA - 40 + 2, i + 5), Point(WI - MA - MA - 40, i), Point(WI - MA - MA - 40, i + 10))
+        move(Point(WI - MA - MA - 40, i + 10))
+    end
+    line(Point(WI - MA - MA - 40, CIRCUITHEIGHT / 2))
+    move(Point(WI - MA - MA - 40, CIRCUITHEIGHT / 2))
+    line(Point(WI - MA - MA - 40 - CIRCUITWIDTH, CIRCUITHEIGHT / 2))
+    move(Point(WI - MA - MA - 40 - CIRCUITWIDTH, CIRCUITHEIGHT / 2))
+    line(Point(WI - MA - MA - 40 - CIRCUITWIDTH, 10))
+    move(Point(WI - MA - MA - 40 - CIRCUITWIDTH, 10))
+    line(Point(WI - MA - MA - 40 - CIRCUITWIDTH + 30, 10))
+    line(Point(WI - MA - MA - 40 - CIRCUITWIDTH - 30, 10))
+    strokepath()
+    #
+    fontsize(20)
+    text(join([string(CCap), "F"]), WI - MA - MA - 40 - CIRCUITWIDTH - 110, 5)
+    text(join([string(LInd), "H"]), WI - MA - MA - 40 - 70, 5)
+end
+
+function drawCharges(n, flag)
+    spawnpx = WI - MA - MA - 40 - CIRCUITWIDTH - 30
+    spawnpy = flag ? -10 : 10
+    sethue("blue")
+    for i in 0:n
+        circle(Point(spawnpx + i * 5, spawnpy), 5, :fill)
+    end
+end
+
+function drawField(k, step)
+    spawn = Point(WI - MA - MA - 40, 0)
+    circle(spawn, abs(k * I(step)), :stroke)
+end
+
+bbuffer = Vector{Point}()
+
+sim = Movie(WI, HE, "LC")
+t = 0
+p = (x, y) -> Point(x * STX, -(y * STY))
+
+function backdrop(scene, framenumber)
+    background("white")
+    origin(Point(MA, HE / 2))
+
+    drawAxis()
+    drawCircuit()
+end
+
+function frame(scene, framenumber)
+    global t
+    if t >= period
+        empty!(bbuffer)
+        t = 0
+    end
+
+    push!(bbuffer, p(t, dI(t)))
+
+    sethue("blue")
+    poly(bbuffer, :stroke)
+    drawCharges(trunc((q(t) / Q) * 12), true)
+    drawCharges(trunc((-q(t) / Q) * 12), false)
+
+    UCond = q(t) / CCap
+    sethue("black")
+    text(join([string(trunc(UCond)), "V"]), WI - MA - MA - CIRCUITWIDTH + 40, 5)
+
+    sethue("green")
+    drawField(5000, t)
+
+    t = t + 0.007
+end
+
+animate(sim, [
+        Scene(sim, backdrop, 0:359),
+        Scene(sim, frame, 0:359,
+            easingfunction=easeinoutcubic,
+            optarg="made with Julia")
+    ],
+    creategif=true)
