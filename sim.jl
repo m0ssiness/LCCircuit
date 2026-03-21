952fe6685e1942e2ab15667e9366d49f4c265d81
m0ssiness <timofej.bozhenko@yandex.ru>
2026-03-21 12:40:58 +0100

    fix: magnetic field vis

--- sim.jl
+++ sim.jl
@@ -2,7 +2,7 @@
 
 using Luxor
 
-include(dirname(pathof(Luxor)) * "/play.jl")
+# include(dirname(pathof(Luxor)) * "/play.jl")
 
 const LInd = 980
 const CCap = 0.00005
@@ -54,7 +54,7 @@ function drawCircuit()
     line(Point(WI - MA - MA - 40, -30))
     move(Point(WI - MA - MA - 40, -30))
     for i in -30:10:20
-        arc2r(Point(WI - MA - MA - 40 + 2, i + 5), Point(WI - MA - MA - 40, i), Point(WI - MA - MA - 40, i + 10))
+        arc2r(Point(WI - MA - MA - 40 + 8, i + 5), Point(WI - MA - MA - 40, i), Point(WI - MA - MA - 40, i + 10))
         move(Point(WI - MA - MA - 40, i + 10))
     end
     line(Point(WI - MA - MA - 40, CIRCUITHEIGHT / 2))
@@ -81,18 +81,50 @@ function drawCharges(n, flag)
     end
 end
 
-function drawField(k, step, multiplier)
-    p1 = Point(WI - MA - MA - 40 + (I(step) * multiplier), -(I(step) * 2.5 * multiplier))
-    m = Point(WI - MA - MA - 40, 0)
-    p2 = Point(WI - MA - MA - 40 + (I(step) * multiplier), (I(step) * 2.5 * multiplier))
+# function drawField(k, step, multiplier)
+#     p1 = Point(WI - MA - MA - 40 + (I(step) * multiplier), -(I(step) * 2.5 * multiplier))
+#     m = Point(WI - MA - MA - 40, 0)
+#     p2 = Point(WI - MA - MA - 40 + (I(step) * multiplier), (I(step) * 2.5 * multiplier))
 
-    evilp1 = Point(WI - MA - MA - 40 - (I(step) * multiplier), -(I(step) * 2.5 * multiplier))
-    evilp2 = Point(WI - MA - MA - 40 - (I(step) * multiplier), (I(step) * 2.5 * multiplier))
+#     evilp1 = Point(WI - MA - MA - 40 - (I(step) * multiplier), -(I(step) * 2.5 * multiplier))
+#     evilp2 = Point(WI - MA - MA - 40 - (I(step) * multiplier), (I(step) * 2.5 * multiplier))
 
-    ellipse(p1, p2, m, :stroke)
-    ngon(Point(WI - MA - MA - 40 + (2 * (I(step) * multiplier)), 0), 8, 3, sign(I(step))*0.5*pi, :fill)
-    ellipse(evilp1, evilp2, m, :stroke)
-    ngon(Point(WI - MA - MA - 40 - (2 * (I(step) * multiplier)), 0), 8, 3, sign(I(step))*0.5*pi, :fill)
+#     ellipse(p1, p2, m, :stroke)
+#     ngon(Point(WI - MA - MA - 40 + (2 * (I(step) * multiplier)), 0), 8, 3, sign(I(step))*0.5*pi, :fill)
+#     ellipse(evilp1, evilp2, m, :stroke)
+#     ngon(Point(WI - MA - MA - 40 - (2 * (I(step) * multiplier)), 0), 8, 3, sign(I(step))*0.5*pi, :fill)
+# end
+
+function drawFieldLine(step, k, n)
+    x = WI - MA - MA - 40 + 8
+    h = Point(x + sign(I(step))*-k*3, -30)
+    b = Point(x + sign(I(step))*-k*3, 30)
+    ha = Point(x - sign(I(step))*-k*3, -30)
+    ba = Point(x - sign(I(step))*-k*3, 30)
+    factor = exp((k-0.4)/3)
+    # println(factor)
+    move(b)
+    line(h)
+    strokepath()
+    ngon(Point(x + sign(I(step))*-k*3 + factor * I(step) * 6000, 0), 8, 3, sign(I(step))*0.5*pi, :fill)
+    move(h)
+    curve(Point(x + sign(I(step))*-k*3 + factor * I(step) * 8000, -60 - abs(3*factor*exp(n + k) * I(step) * 50)), Point(x + sign(I(step))*-k*3 + factor * I(step) * 8000, 60 + abs(3*factor*exp(n + k) * I(step) * 50)), b)
+    strokepath()
+    move(ba)
+    line(ha)
+    strokepath()
+    ngon(Point(x - sign(I(step))*-k*3 - factor * I(step) * 6000, 0), 8, 3, sign(I(step))*0.5*pi, :fill)
+    move(ha)
+    curve(Point(x - sign(I(step))*-k*3 - factor * I(step) * 8000, -60 - abs(3*factor*exp(n + k) * I(step) * 50)), Point(x - sign(I(step))*-k*3 - factor * I(step) * 8000, 60 + abs(3*factor*exp(n + k) * I(step) * 50)), ba)
+    strokepath()
+end
+
+function drawField(step, n)
+    for i in 1:n
+        if abs(exp((i-1)/3) * (I(step) * 8000)) > 50*exp(i-2.2)
+            drawFieldLine(step, -i + 1, n)
+        end
+    end
 end
 
 bbuffer = Vector{Point}()
@@ -120,17 +152,20 @@ function frame(scene, framenumber)
 
     sethue("blue")
     poly(bbuffer, :stroke)
-    drawCharges(round(Int, (q(t) / Q) * 12), true)
-    drawCharges(round(Int, (-q(t) / Q) * 12), false)
+    drawCharges(round(Int, (q(t) / Q) * 12, RoundDown), true)
+    drawCharges(round(Int, (-q(t) / Q) * 12, RoundDown), false)
 
     UCond = q(t) / CCap
     sethue("black")
     text(join([string(round(Int, UCond)), "V"]), WI - MA - MA - CIRCUITWIDTH + 40, 5)
+    text(join(["Stromstärke ", string(round(Int, I(t)*1000)), "mA"]), WI - MA - MA - CIRCUITWIDTH - 370, 300)
+    text(" ~ Magnetfeldstärke B", WI - MA - MA - CIRCUITWIDTH - 200, 300)
 
     sethue("green")
-    drawField(5000, t, 2000)
-    drawField(5000, t, 1300)
-    drawField(5000, t, 500)
+    # drawField(5000, t, 2000)
+    # drawField(5000, t, 1300)
+    # drawField(5000, t, 500)
+    drawField(t, 4)
 
     t = t + 0.007
 end
